480000336401aa9ea8fc93b4bdd7daa2a603de9d
kvitebjorn <harintonkairu@gmail.com>
2024-08-11 20:15:18 -0600

    find_duplicate_in_array solution

--- array/find_duplicate_in_array.go
+++ array/find_duplicate_in_array.go
@@ -1,6 +1,7 @@
 package array
 
 // FindDuplicate solves the problem in O(n) time and O(1) space.
+/*
 func FindDuplicate(list []int) int {
 	for _, item := range list {
 		itemIndex := item - 1
@@ -8,6 +9,34 @@ func FindDuplicate(list []int) int {
 			return list[itemIndex] * -1
 		}
 		list[itemIndex] *= -1
+	}
+	return -1
+}
+*/
+
+// 1st iteration:
+// Oops, didn't realize that `list` is guaranteed to be the sequence 1,2..n :P
+// in other words, a map is not required, and violated the O(1) space req anyway...
+/*
+func FindDuplicate(list []int) int {
+	M := make(map[int]bool)
+	for _, elem := range list {
+		_, ok := M[elem]
+		if !ok {
+			M[elem] = true
+		} else {
+			return elem
+		}
+	}
+	return -1
+}
+*/
+
+func FindDuplicate(list []int) int {
+	for idx, elem := range list {
+		if idx+1 != elem {
+			return elem
+		}
 	}
 	return -1
 }
